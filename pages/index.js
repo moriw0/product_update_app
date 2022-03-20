288028c7e8d630c6a275bcee831e2a33a132f783
shota <tontonka2@gmail.com>
2022-03-21 06:28:34 +0900

    complete

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import { useState, useMemo } from "react";
-import { Heading, Page, Button, Card, Stack, TextField, DataTable, EmptyState } from "@shopify/polaris";
+import { useState, useMemo, useCallback } from "react";
+import { Heading, Page, Button, Card, Stack, TextField, DataTable, EmptyState, Toast, Frame } from "@shopify/polaris";
 import { ResourcePicker } from "@shopify/app-bridge-react";
+import { useMutation } from "react-apollo";
+
+import { ProductUpdateMutation } from "../grapdhql/ProductUpdate";
 
 const Index = () => {
   const [appendToTitle, setAppendToTitle] = useState('');
@@ -9,55 +12,94 @@ const Index = () => {
   const [products, setProducts] = useState([]);
   const [showToast, setShowToast] = useState(false);
 
+  const [updateProduct] = useMutation(ProductUpdateMutation);
+
   const productTableDisplayData = useMemo(() => products.map((product) => [
     product.id,
     product.title,
     `${product.title}${appendToTitle}`,
     product.descriptionHtml,
-    `${product.descriptionHtml}$${appendToDescription}`
+    `${product.descriptionHtml}${appendToDescription}`
   ]), [products, appendToTitle, appendToDescription]);
 
+  const submitHandler = useCallback(() => {
+    let count = 0;
+    const runMutation = (product) => {
+      updateProduct({
+        variables: {
+          input: {
+            descriptionHtml: `${product.descriptionHtml}${appendToDescription}`,
+            title: `${product.title}${appendToTitle}`,
+            id: product.id
+          }
+        }
+      }).then((data) => {
+        console.log('Updated Product', count, data);
+        count++;
+        if(products[count]) runMutation(products[count])
+        else {
+          console.log('Updates Complete')
+          setShowToast(true);
+        }
+      })
+    }
+    runMutation(products[count]);
+  }, [products, appendToTitle, appendToDescription]);
+
+  const toastMarkup = showToast ? 
+  <Toast 
+    content="Update Successful"
+    onDismiss={() => setShowToast(false)}
+    onduration={4000}
+  /> : null;
+
   return (
-    <Page>
-      <Heading>
-        Update Product
-      </Heading>
-      <Card>
-        <Card.Section>
-          <Stack vertical>
-            <TextField
-              label="Append to title"
-              value={appendToTitle}
-              onChange={setAppendToTitle}
-            />
-            <TextField
-              label="Append to description"
-              value={appendToDescription}
-              onChange={setAppendToDescription}
-              multiline={3}
-            />
-            <ResourcePicker
-              resourceType="Product"
-              showVariant={false}
-              open={pickerOpen}
-              onSelection={(resources) => {
-                console.log(resources);
-                setProducts(resources.selection);
-              }}
-            />
-            <Button primary onClick={() => setPickerOpen(true)}>Select Products</Button>
-          </Stack>
-        </Card.Section>
-        <Card.Section>
-          {productTableDisplayData.length ? <DataTable
-            columnContentTypes={['text', 'text', 'text', 'text', 'text']}
-            headings={['ID', 'Old Title', 'New Title', 'Old Description', 'New Description']}
-            rows={productTableDisplayData}
-          /> : <EmptyState heading="No products selected" /> }
-        </Card.Section>
-      </Card>
-    </Page>
-  )
+    <Frame>
+      <Page>
+        <Heading>
+          Update Product
+        </Heading>
+        <Card>
+          <Card.Section>
+            <Stack vertical>
+              <TextField
+                label="Append to title"
+                value={appendToTitle}
+                onChange={setAppendToTitle}
+              />
+              <TextField
+                label="Append to description"
+                value={appendToDescription}
+                onChange={setAppendToDescription}
+                multiline={3}
+              />
+              <ResourcePicker
+                resourceType="Product"
+                showVariant={false}
+                open={pickerOpen}
+                onSelection={(resources) => {
+                  console.log(resources);
+                  setProducts(resources.selection);
+                }}
+              />
+              <Button primary onClick={() => setPickerOpen(true)}>Select Products</Button>
+            </Stack>
+          </Card.Section>
+          <Card.Section>
+            {productTableDisplayData.length ? <DataTable
+              columnContentTypes={['text', 'text', 'text', 'text', 'text']}
+              headings={['ID', 'Old Title', 'New Title', 'Old Description', 'New Description']}
+              rows={productTableDisplayData}
+            /> : <EmptyState heading="No products selected" /> }
+          </Card.Section>
+          <Card.Section>
+            <Button primary onClick={submitHandler} disabled={!products.length}>Submit</Button>
+          </Card.Section>
+        </Card>
+        {toastMarkup}
+      </Page>
+    </Frame>
+  ) 
 };
 
 export default Index;
